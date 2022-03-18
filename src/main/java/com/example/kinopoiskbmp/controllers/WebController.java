a3dba9af6da7ca31397ef508ad000e7fd3771f7d
dzyubakanton <nesladkiy@icloud.com>
2022-03-19 01:33:13 +0300

    warnings in WebController.java

--- src/main/java/com/example/kinopoiskbmp/controllers/WebController.java
+++ src/main/java/com/example/kinopoiskbmp/controllers/WebController.java
@@ -5,19 +5,14 @@ import com.example.kinopoiskbmp.entities.Content;
 import com.example.kinopoiskbmp.entities.ContentType;
 import com.example.kinopoiskbmp.entities.Genre;
 import com.example.kinopoiskbmp.entities.ReviewKey;
-import com.example.kinopoiskbmp.repositories.ClientRepository;
-import com.example.kinopoiskbmp.services.ClientService;
 import com.example.kinopoiskbmp.services.ContentService;
 import com.example.kinopoiskbmp.services.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/kinopoisk")
@@ -31,28 +26,28 @@ public class WebController {
 
 
     @GetMapping(value = "/getContentTypes")
-    public List<ContentType> getContentTypes(){
+    public List<ContentType> getContentTypes() {
         return contentService.getContentTypes();
     }
 
     @GetMapping("/getContentGenre")
-    public List<Genre> getContentGenre(){
+    public List<Genre> getContentGenre() {
         return contentService.getContentGenres();
     }
 
     @GetMapping("/getContent/{type}/{genre}")
-    public List<Content> getContent(@PathVariable(name = "type") String type, @PathVariable(name = "genre") String genre){
+    public List<Content> getContent(@PathVariable(name = "type") String type, @PathVariable(name = "genre") String genre) {
         return contentService.getContentTypeByContentTypeOrGenre(genre, type);
     }
 
     @PostMapping(value = "/sendReview", consumes = "application/json", produces = "application/json")
-    public ReviewKey sendReview(@RequestBody RequestReview requestReview){
-//        return new HashMap<String, String>() {{"fdf","fdfd"},{"fdf", fdf };
+    public HashMap<String, String> sendReview(@RequestBody RequestReview requestReview) {
+        ReviewKey reviewKey = reviewService.sendReview(requestReview);
+        return new HashMap<>() {{
+            put("contentId", reviewKey.getContent().getId().toString());
+            put("clientId", reviewKey.getClient().getId().toString());
+        }};
     }
-
-
-
-    
 
 
 }
